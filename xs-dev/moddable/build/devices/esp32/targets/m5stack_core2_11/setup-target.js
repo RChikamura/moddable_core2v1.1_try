892b5c7ec70cbe08de08f641c59f4576231a64a0
RChikamura <himitunandakara@gmail.com>
2024-02-26 01:06:55 +0900

    push test

---
+++ xs-dev/moddable/build/devices/esp32/targets/m5stack_core2_11/setup-target.js
@@ -0,0 +1,271 @@
+/*
+ * Copyright (c) 2020-2023  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ * 
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ 
+import AXP2101 from "axp2101"; //変更
+import MPU6886 from "mpu6886";
+import AudioOut from "pins/audioout";
+import Resource from "Resource";
+import Timer from "timer";
+import config from "mc/config";
+import I2C from "pins/i2c";
+
+const INTERNAL_I2C = Object.freeze({
+	sda: 21,
+	scl: 22
+});
+
+const state = {
+  handleRotation: nop,
+};
+
+globalThis.Host = {
+  Backlight: class {
+    constructor(brightness = 100) {
+      this.write(brightness);
+    }
+    write(value) {
+      if (undefined !== globalThis.power)
+        globalThis.power.brightness = value;
+    }
+    close() {}
+  }
+}
+
+class M5Core2Button {		// M5StackCoreTouch calls write when button changes 
+	#value = 0;
+	read() {
+		return this.#value;
+	}
+	write(value) {
+		if (this.#value === value)
+			return;
+		this.#value = value;
+		this.onChanged?.();
+	}
+}
+
+export default function (done) {
+	// buttons
+	globalThis.button = {
+		a: new M5Core2Button,
+		b: new M5Core2Button,
+		c: new M5Core2Button,
+	};
+
+	// power
+	globalThis.power = new Power();
+
+	// speaker
+	power.speaker.enable = true;
+
+	// start-up sound
+	if (config.startupSound) {
+    const speaker = new AudioOut({streams: 1});
+		speaker.callback = function () {
+			this.stop();
+			this.close();
+			Timer.set(this.done);
+		};
+		speaker.done = done;
+		done = undefined;
+
+		speaker.enqueue(0, AudioOut.Samples, new Resource(config.startupSound));
+		speaker.enqueue(0, AudioOut.Callback, 0);
+		speaker.start();
+	}
+
+  // vibration
+  globalThis.vibration = {
+    read: function () {
+      return power.vibration.enable;
+    },
+    write: function (v) {
+      power.vibration.enable = v;
+    },
+  };
+
+  if (config.startupVibration) {
+    vibration.write(true);
+    Timer.set(() => {
+      vibration.write(false);
+    }, config.startupVibration);
+  }
+
+  // accelerometer and gyrometer
+  const test = new I2C({...INTERNAL_I2C, address: 0x68, throw: false});
+  test.write(0x75, false);
+  const ok = test.read(1);
+  test.close();
+  if (undefined !== ok) {
+    state.accelerometerGyro = new MPU6886(INTERNAL_I2C);
+
+    globalThis.accelerometer = {
+      onreading: nop,
+    };
+
+    globalThis.gyro = {
+      onreading: nop,
+    };
+
+    accelerometer.start = function (frequency) {
+      accelerometer.stop();
+      state.accelerometerTimerID = Timer.repeat((id) => {
+        state.accelerometerGyro.configure({
+          operation: "accelerometer",
+        });
+        const sample = state.accelerometerGyro.sample();
+        if (sample) {
+          state.handleRotation(sample);
+          accelerometer.onreading(sample);
+        }
+      }, frequency);
+    };
+
+    gyro.start = function (frequency) {
+      gyro.stop();
+      state.gyroTimerID = Timer.repeat((id) => {
+        state.accelerometerGyro.configure({
+          operation: "gyroscope",
+        });
+        const sample = state.accelerometerGyro.sample();
+        if (sample) {
+          let { x, y, z } = sample;
+          const temp = x;
+          x = y * -1;
+          y = temp * -1;
+          z *= -1;
+          gyro.onreading({
+            x,
+            y,
+            z,
+          });
+        }
+      }, frequency);
+    };
+
+    accelerometer.stop = function () {
+      if (undefined !== state.accelerometerTimerID)
+        Timer.clear(state.accelerometerTimerID);
+      delete state.accelerometerTimerID;
+    };
+
+    gyro.stop = function () {
+      if (undefined !== state.gyroTimerID) Timer.clear(state.gyroTimerID);
+      delete state.gyroTimerID;
+    };
+  }
+
+  // autorotate
+  if (config.autorotate && globalThis.Application && globalThis.accelerometer) {
+    state.handleRotation = function (reading) {
+      if (globalThis.application === undefined) return;
+
+      if (Math.abs(reading.y) > Math.abs(reading.x)) {
+        if (reading.y < -0.7 && application.rotation != 180) {
+          application.rotation = 180;
+        } else if (reading.y > 0.7 && application.rotation != 0) {
+          application.rotation = 0;
+        }
+      } else {
+        if (reading.x < -0.7 && application.rotation != 270) {
+          application.rotation = 270;
+        } else if (reading.x > 0.7 && application.rotation != 90) {
+          application.rotation = 90;
+        }
+      }
+    };
+    accelerometer.start(300);
+  }
+
+  done?.();
+}
+
+// 回路図に合わせてポートを変更
+class Power extends AXP2101 {
+  constructor() {
+    super(INTERNAL_I2C);
+
+    this.writeByte(0x27, 0x00); // PowerKey Hold=1sec / PowerOff=4sec
+    this.writeByte(0x10, 0x30); // PMU common config (internal off-discharge enable)
+    this.writeByte(0x12, 0x00); // BATFET disable
+    this.writeByte(0x68, 0x01); // Battery detection enabled.
+    this.writeByte(0x69, 0x13); // CHGLED setting
+    this.writeByte(0x99, 0x00); // DLDO1 set 0.5v (vibration motor)
+
+    // main power line
+    this._dcdc1.voltage = 3350;
+    this._dcdc3.voltage = 3350;
+    this.chargeCurrent = AXP2101.CHARGE_CURRENT.Ch_100mA;
+
+    // LCD
+    this.lcd = this._bldo1;
+    this.lcd.voltage = 2800;
+
+    // internal LCD logic
+    this._aldo4.voltage = 3300;
+    this._aldo4.enable = true;
+
+    // Vibration
+    this.vibration = this._dldo1;
+    this.vibration.voltage = 2000;
+
+    // Speaker
+    this.speaker = this._aldo3;
+    this.speaker.voltage = 3300;
+
+    // AXP192 GPIO4 -> AXP2101 ALDO2
+    this._aldo2.voltage = 3300;
+    this.resetLcd();
+    this.busPowerMode = 0; //  bus power mode_output
+    Timer.delay(200);
+  }
+
+  resetLcd() {
+    this._aldo2.enable = false;
+    Timer.delay(20);
+    this._aldo2.enable = true;
+  }
+
+  set busPowerMode(mode) {
+    if (mode == 0) {
+      this._bldo2.voltage = 3300;
+      this._bldo2.enable = true;
+    } else {
+      this._bldo2.enable = false;
+    }
+  }
+  
+  // value 0 - 100 %
+  set brightness(value) {
+    if (value <= 0)
+      value = 2500;
+    else if (value >= 100)
+      value = 3300;
+    else
+      value = (value / 100) * 800 + 2500;
+    this.lcd.voltage = value;
+  }
+  
+  get brightness() {
+    return (this.lcd.voltage - 2500) / 800 * 100;
+  }
+}
+
+function nop() {}
---
+++ xs-dev/moddable/modules/drivers/axp2101/axp2101.js
@@ -0,0 +1,350 @@
+/*
+ *   Copyright (c) 2019 Shinya Ishikawa
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ *
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+import SMBus from "pins/smbus";
+
+// なかったのでaxp192.jsのコードを参考に追加
+class DCDC {
+  #parent;
+  #register;
+  constructor({ register, parent }) {
+    this.#parent = parent;
+    this.#register = register;
+  }
+  set voltage(v) {
+    let vtemp; // 電圧>設定値の計算結果を代入する変数
+	let mask = 0x80; // 電圧設定値をレジスタに書き込む際に使用。電圧設定のビットを0とし、それ以外を１とする。
+	switch (this.#register){ // AXP2101にはDCDC1~5までがあるらしい(少なくとも4までは回路図にある)が、全部設定が違うので分岐
+	  case 0x82: // DCDC1
+	    vtemp = (v < 1500) ? 0 : (v < 3400) ? (v - 1500) / 100 : 19;
+		mask = 0xE0; // 下位5bitが電圧設定
+		break;
+	  case 0x83: // DCDC2
+	    vtemp = (v < 500) ? 0 : (v <= 1200) ? (v - 500) / 10 : (v <= 1540) ? (v - 1200) / 20 + 70 : 87;
+		break;
+	  case 0x84: // DCDC3
+        vtemp = (v < 500) ? 0 : (v <= 1200) ? (v - 500) / 10 : (v <= 1540) ? (v - 1200) / 20 + 70 : (v < 1600) ? 87 : (v <= 3400) ? (v - 1600) / 20 + 88 : 107;
+		break;
+	  case 0x85: // DCDC4
+	    vtemp = (v < 500) ? 0 : (v <= 1200) ? (v - 500) / 10 : (v <= 1840) ? (v - 1200) / 20 + 70 : 102;
+		break;
+	  case 0x86: // DCDC5
+	    vtemp = (v < 1400) ? 0 : (v < 3700) ? (v - 1400) / 100 : 23;
+		mask = 0xE0; // 下位5bitが電圧設定
+		break;
+	  default: // どれでもなければとりあえず0, maskもとりあえず標準(下位7bit)
+	    vtemp = 0;
+		break;
+	}
+	const vdata = vtemp;
+    this.#parent.writeByte(this.#register, (this.#parent.readByte(this.#register) & mask) | (vdata & ~mask));
+  }
+  get voltage() {
+	let v = 0;
+	let vdata = this.#parent.readByte(this.#register);
+
+	switch (this.#register){ // AXP2101にはDCDC1~5までがあるらしい(少なくとも4までは回路図にある)が、全部設定が違うので分岐
+	  case 0x82: // DCDC1
+	    vdata &= 0x1f;
+		v = vdata * 100 + 1500;
+		break;
+	  case 0x83: // DCDC2
+	  case 0x85: // DCDC4 DCDC2とは上限値違いなだけのため、共通化
+	    vdata &= 0x7f;
+	    v = (vdata <= 70) ? vdata * 10 + 500 : (vdata - 70) * 20 + 1200;
+		break;
+	  case 0x84: // DCDC3
+	    vdata &= 0x7f;
+	    v = (vdata <= 70) ? vdata * 10 + 500 : (vdata <= 87) ? (vdata - 70) * 20 + 1200 : (vdata - 88) * 100 + 1600;
+		break;
+	  case 0x86: // DCDC5
+	    vdata &= 0x1f;
+		v = vdata * 100 + 1400;
+		break;
+	  default: // どれでもなければ何もしない(値は0になる)
+		break;
+	}
+
+    return v;
+  }
+}
+
+class DLEDO { // 元のLDO設定。修正前のコードで使っていたので、一応残してはおく
+  #parent;
+  #register;
+  #offsetV;
+  #offsetEn;
+  constructor({ register, parent, offsetV, offsetEn }) {
+    this.#parent = parent;
+    this.#register = register;
+    this.#offsetV = offsetV;
+    this.#offsetEn = offsetEn;
+  }
+
+  set voltage(v) {
+    const vdata = v > 3300 ? 15 : v / 100 - 18;
+    const mask = ~(0xff << this.#offsetV);
+    this.#parent.writeByte(
+      this.#register,
+      (this.#parent.readByte(this.#register) & mask) | (vdata << this.#offsetV)
+    );
+  }
+
+  get voltage() {
+    return (
+      ((this.#parent.readByte(this.#register) >> this.#offsetV) + 18) * 100
+    );
+  }
+
+  set enable(enable) {
+    const mask = 0x01 << this.#offsetEn;
+    if (enable) {
+      this.#parent.writeByte(0x12, this.#parent.readByte(0x12) | mask);
+    } else {
+      this.#parent.writeByte(0x12, this.#parent.readByte(0x12) & ~mask);
+    }
+  }
+
+  get enable() {
+    return Boolean((this.#parent.readByte(0x12) >> this.#offsetEn) & 1);
+  }
+}
+
+class LDO {
+  #parent;
+  #register;
+  #registerEn; // 追加 ただし、dldo2だけ0x91, それ以外は0x90なので、registerの値から条件演算子で選択する方式にする
+  //#offsetV; 全部下位5bitを使用しているので、使わないことにする
+  #offsetEn;
+  // offsetVを削除し、#registerEnの自動判別コードを追加
+  constructor({ register, parent, offsetEn }) {
+    this.#parent = parent;
+    this.#register = register;
+    this.#registerEn = (register == 0x9A) ? 0x91 : 0x90;
+    this.#offsetEn = offsetEn;
+  }
+  
+  // 以下、AXP2101用の設定に直す。cpusldoとdldo2は上限が他より低い
+  set voltage(v) {
+    let vtemp; // 電圧>設定値の計算結果を代入する変数
+    const vdata = (this.#register == 0x98 || this.#register == 0x9A) ? (v < 500 ? 0 : v > 1400 ? 19 : (v - 500) / 50) : (v < 500 ? 0 : v > 3500 ? 30 : (v - 500) / 100);
+    //const mask = ~(0xff << this.#offsetV); 使わない
+    this.#parent.writeByte( this.#register, (this.#parent.readByte(this.#register) & 0x1f) | vdata); //下位5bitに電圧設定を書き込み
+  }
+
+  get voltage() {
+	let vdata = this.#parent.readByte(this.#register) & 0x1f;
+	let v = (this.#register == 0x98 || this.#register == 0x9A) ? vdata * 50 + 500 : vdata * 100 + 500;
+
+    return v;
+  }
+
+  set enable(enable) {
+    const mask = 0x01 << this.#offsetEn;
+    if (enable) {
+      this.#parent.writeByte(this.#registerEn, this.#parent.readByte(this.#registerEn) | mask);
+    } else {
+      this.#parent.writeByte(this.#registerEn, this.#parent.readByte(this.#registerEn) & ~mask);
+    }
+  }
+
+  get enable() {
+    return Boolean((this.#parent.readByte(this.#registerEn) >> this.#offsetEn) & 1);
+  }
+}
+
+export default class AXP2101 extends SMBus {
+  constructor(it) {
+    //super({ address: 0x34, ...it });
+    //this._dledo1 = new DLEDO({ register: 0x34, parent: this, offsetV: 5, offsetEn: 7}); //もとからあったLDO設定(改行をなくしただけ)
+
+	//データシートとaxp192.jsのコードを参考に追加
+    this._dcdc1 = new DCDC({ register: 0x82, parent: this });
+    this._dcdc2 = new DCDC({ register: 0x83, parent: this });
+    this._dcdc3 = new DCDC({ register: 0x84, parent: this });
+	this._aldo1 = new LDO({ register: 0x92, parent: this, offsetEn: 0 });
+	this._aldo2 = new LDO({ register: 0x93, parent: this, offsetEn: 1 });
+	this._aldo3 = new LDO({ register: 0x94, parent: this, offsetEn: 2 });
+	this._aldo4 = new LDO({ register: 0x95, parent: this, offsetEn: 3 });
+	this._bldo1 = new LDO({ register: 0x96, parent: this, offsetEn: 4 });
+	this._bldo2 = new LDO({ register: 0x97, parent: this, offsetEn: 5 });
+	this._dldo1 = new LDO({ register: 0x99, parent: this, offsetEn: 7 });
+  }
+
+  isACINExist() {
+    return Boolean(this.readByte(0x00) & 0b10000000);
+  }
+
+  isACINAvailable() {
+    return Boolean(this.readByte(0x00) & 0b01000000);
+  }
+
+  isVBUSExist() {
+    return Boolean(this.readByte(0x00) & 0b00100000);
+  }
+
+  isVBUSAvailable() {
+    return Boolean(this.readByte(0x00) & 0b00010000);
+  }
+
+  /**
+   * @brief Get battery current direction
+   * @returns true: battery charging, false: battery discharging
+   */
+  getBatteryCurrentDirection() {
+    return Boolean(this.readByte(0x00) & 0b00000100);
+  }
+
+  isAXP173OverTemperature() {
+    return Boolean(this.readByte(0x01) & 0b10000000);
+  }
+
+  isCharging() {
+    return Boolean(this.readByte(0x01) & 0b01000000);
+  }
+
+  isBatteryExist() {
+    return Boolean(this.readByte(0x01) & 0b00100000);
+  }
+
+  isChargeCSmaller() {
+    return Boolean(this.readByte(0x01) & 0b00000100);
+  }
+
+  powerOff() {
+    this.writeByte(0x10, this.readByte(0x10) | 0b00000001);
+  }
+
+  setChargeEnable(enable) {
+    if (enable) {
+      this.writeByte(0x33, this.readByte(0x33) | 0b10000000);
+    } else {
+      this.writeByte(0x33, this.readByte(0x33) | 0b10000000);
+    }
+  }
+
+  setChargeCurrent(state) {
+    this.writeByte(0x33, (this.readByte(0x33) & 0xf0) | (state & 0x0f));
+  }
+
+  setADCEnable(channel, enable) {
+    const mask = 0x01 << channel;
+    if (enable) {
+      this.writeByte(0x82, this.readByte(0x82) | mask);
+    } else {
+      this.writeByte(0x82, this.readByte(0x82) & ~mask);
+    }
+  }
+
+  setCoulometerEnable(channel, enable) {
+    const mask = 0x01 << channel;
+    if (enable) {
+      this.writeByte(0x12, this.readByte(0x12) | mask);
+    } else {
+      this.writeByte(0x12, this.readByte(0x12) & ~mask);
+    }
+  }
+
+  _getCoulometerCharge() {
+    return this.readBlock(0xb0, 4);
+  }
+
+  _getCoulometerDischarge() {
+    return this.readBlock(0xb4, 4);
+  }
+
+  getCoulometerCurrent() {
+    const coin = this._getCoulometerCharge();
+    const coout = this._getCoulometerDischarge();
+    // data = 65536 * current_LSB * (coin - coout) / 3600 / ADC rate
+    return (65536 * 0.5 * (coin - coout)) / 3600 / 25;
+  }
+
+  getBatteryVoltage() {
+    const ADCLSB = 1.1 / 1000.0;
+  }
+
+  getBatteryLevel() {
+    const voltage = this.getBatteryVoltage();
+    const percentage = voltage < 3.248088 ? 0 : (voltage - 3.120712) * 100;
+    return percentage <= 100 ? percentage : 100;
+  }
+
+  getBatteryPower() {
+    const VOLTAGE_LSB = 1.1;
+    const CURRENT_LCS = 0.5;
+    return (VOLTAGE_LSB * CURRENT_LCS * this._read24Bit(0x70)) / 1000.0;
+  }
+
+  getVBUSVoltage() {
+    const ADC_LSB = 1.7 / 1000.0;
+    return ADC_LSB * this._read12Bit(0x5a);
+  }
+
+  getVBUSCurrent() {
+    const ADC_LSB = 0.375;
+    return ADC_LSB * this._read12Bit(0x5c)
+  }
+
+  getAXP173Temperature() {
+    const ADC_LSB = 0.1;
+    const OFFSET_DEG_C = -144.7;
+    return ADC_LSB * this._read12Bit(0x5e) + OFFSET_DEG_C
+  }
+
+  getTSTemperature() {
+    const ADC_LSB = 0.1;
+    const OFFSET_DEG_C = -144.7;
+    return ADC_LSB * this._read12Bit(0x62) + OFFSET_DEG_C
+  }
+
+  _read24Bit(address) {
+    const buff = this.readBlock(address, 3);
+    return (buff[0] << 16) + (buff[1] << 8) + buff[2];
+  }
+
+  _read12Bit(address) {
+    const buff = this.readBlock(address, 2);
+    return (buff[0] << 4) + buff[1];
+  }
+}
+
+AXP2101.COULOMETER_CTRL = {
+  COULOMETER_RESET: 0x05,
+  COULOMETER_PAUSE: 0x06,
+  COULOMETER_ENABLE: 0x07,
+};
+AXP2101.CHARGE_CURRENT = {
+  Ch_100mA: 0b0000,
+  Ch_190mA: 0b0001,
+  Ch_280mA: 0b0010,
+  Ch_360mA: 0b0011,
+  Ch_450mA: 0b0100,
+  Ch_550mA: 0b0101,
+  Ch_630mA: 0b0110,
+  Ch_700mA: 0b0111,
+  Ch_780mA: 0b1000,
+  Ch_880mA: 0b1001,
+  Ch_960mA: 0b1010,
+  Ch_1000mA: 0b1011,
+  Ch_1080mA: 0b1100,
+  Ch_1160mA: 0b1101,
+  Ch_1240mA: 0b1110,
+  Ch_1320mA: 0b1111,
+};
